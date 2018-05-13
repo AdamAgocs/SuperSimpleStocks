c65ad52bbc1089dc9b9c3c763023206c2a24de58
Adam Agocs <adam.agocs@cern.ch>
2018-05-13 19:17:35 +0200

    Finished:  - tests  - simulation  - comments

--- stocks.py
+++ stocks.py
@@ -10,7 +10,12 @@ from collections import MutableMapping
 
 class StockException(Exception):
     def __init__(self, message_):
-        self.message = message_
+        super(StockException, self).__init__(message_)
+
+class StockManagerException(Exception):
+    def __init__(self, message_):
+        super(StockManagerException, self).__init__(message_)
+
 
 class Stock(ABC):
     '''
@@ -50,22 +55,58 @@ class Stock(ABC):
         raise StockException('Abstract function of Stock Abstract class')
         
 class PreferredStock(Stock):
+    '''
+    Class for handling Preferred Stocks.
+    '''
     def __init__(self, symbol_, last_dividend_, fixed_dividend_, par_value_):
+        '''
+        Constructor
+         
+        @symbol_ - Symbol of the given stock
+        @last_dividend_ - The last dividend value of the stock 
+        @fixed_dividend_ - The fixed dividend value of the stock
+        @par_value_ - The par value of the stock
+        '''
         super(PreferredStock, self).__init__(symbol_, last_dividend_, par_value_)
         self.fixed_dividend = float(fixed_dividend_)
         self.par_value = par_value_
         
     def dividend_yield(self, market_price_):
-        return self.fixed_dividend * self.par_value / market_price_
+        '''
+        See at Stock class
+        
+        The used formula is \frac{fixed dividend * par value}{market price}
+        '''
+        return self.fixed_dividend * self.par_value / market_price_ if market_price_ != 0 else 0
 
 class CommonStock(Stock):
+    '''
+    Class for handling Common Stocks
+    '''
     def __init__(self, symbol_, last_dividend_, par_value_):
+        '''
+        Constuctor 
+        
+        @symbol_ - The symbol of the given stock
+        @last_dividend_ - The last dividend value of the given stock
+        @par_value_ - Par value of the given stock
+        '''
         super(CommonStock, self).__init__(symbol_, last_dividend_, par_value_)
         
-    def dividend_yield(self, market_price):
-        return self.last_dividend / market_price
+    def dividend_yield(self, market_price_):
+        '''
+        See at Stock class
+        
+        The used formula is \frac{last dividend}{market price}
+        '''
+        return (self.last_dividend / market_price_) if market_price_ != 0 else 0
 
 class _SingletonStockManager(type):
+    '''
+    The meta class of Stock Manager.
+    This is an elegant way to handle singletons.
+    '''
+    
     _instance = None
     def __call__(cls):
         if cls._instance == None:
@@ -73,34 +114,92 @@ class _SingletonStockManager(type):
         return cls._instance 
     
 class _CombinedSingletonStockManager(ABCMeta, _SingletonStockManager):
+    '''
+    Because Stock Manager is a subclass of MutableMapping Abstract Class and MutableMapping's meta class
+    is ABCMeta, we have to combine our meta classes. Just for technical purpose!!!
+    '''
     pass
 
 class StockManager(MutableMapping, metaclass=_CombinedSingletonStockManager):
+    '''
+    Stock Manager class.
+    1) Subclass of MutableMapping
+    2) Meta classes: ABCMeta and _SingletonStockManager
+    3) Singleton
+    '''
     def __init__(self):
+        '''
+        Constructor for initialisation
+        '''
         self.__stocks = {}
-        self.__type = {'common' : CommonStock,
-            'preferred' : PreferredStock,
+        self.__type = {'Common' : CommonStock,
+            'Preferred' : PreferredStock,
         }
     
     def __iter__(self):
+        '''
+        Inherited abstract function - iterator
+        
+        @return - Iterator on the stocks symbols!!!!
+        '''
         return iter(self.__stocks)
 
     def __len__(self):
+        '''
+        Inherited abstract function - length
+        
+        @return - Number of stored stocks
+        '''
         return len(self.__stocks)
     
     def __getitem__(self, key):
+        '''
+        Inherited abstract function - getter
+        
+        @return - give back the corresponding value of a key
+        '''
         return self.__stocks[key]
     
     def __setitem__(self, key, value):
+        '''
+        Inherited abstract function - setter
+        '''
         if isinstance(value, Stock):
             self.__stocks[key] = value
+        else:
+            raise StockManager('Invalid Value')
             
     def __delitem__(self, key):
+        '''
+        Inherited abstract function - delete
+        '''
         del self.__stocks[key]
-        
+    
+    
     def create_stocks(self, type_, **kwargs):
-        self.__stocks[kwargs['symbol_']] = self.__type[type_](**kwargs)
+        '''
+        Generate single stock
         
+        @type_ - Type of the stock (Common or Preferred)
+        @kwargs - Attributes of the given stock
+        '''
+        self.__stocks[kwargs['symbol_']] = self.__type[type_](**kwargs)
+    
+        
+    def create_stocks_header(self, header, stocks):
+        '''
+        Generate multiple stocks
+        
+        @header - The attribute list of the stocks
+        @stocks - List of stocks
+        '''
+        for stock in stocks:
+            input = dict(filter(lambda x: x[1] != '', zip(header, stock)))
+            self.create_stocks(input.pop('type'), **input)
+    
     @classmethod
     def _clear(cls):
+        '''
+        Class method for testing if the class is singleton or not 
+        '''
         cls._instance = None 
--- trades.py
+++ trades.py
@@ -9,49 +9,150 @@ from abc import ABCMeta
 from collections import Iterable
 from pandas import DataFrame, Timestamp, Timedelta
 from numpy import array
+from math import log, exp
 from stocks import StockManager
 
 class TradeManagerException(Exception):
     def __init__(self, message_):
         super(TradeManagerException, self).__init__(message_)
         
-class Trade(dict):
-    def __init__(self, stock_, timestamp_, quantity_, buy_or_sell_, trade_price_):
-        self['stock'] = str(stock_)
-        self['timestamp'] = Timestamp(timestamp_)
-        self['quantity'] = int(quantity_)
-        self['buy_or_sell'] = bool(buy_or_sell_)
-        self['trade_price'] = int(trade_price_)
+class TradeException(Exception):
+    def __init__(self, message_):
+        super(TradeException, self).__init__(message_)
 
-class _SingletonTradeManager(type):
-    _instance = None
+class Trade(dict):
+    '''
+    Class of Trade
+    '''
+    __sm = StockManager()
     
+    BUY = True
+    SELL = False
+    
+    def __init__(self, stock_symbol_, timestamp_, quantity_, buy_or_sell_, trade_price_):
+        '''
+        Constructor
+        
+        @stock_symbol_ - Symbol of the given stock
+        @timestamp_ - Storing the time of the trade
+        @quantity_ - Quantity of the stock
+        @buy_or_sell_ - The stocks are bought or sold
+        @trade_price_ - The price of the stocks
+        '''
+        
+        try:
+            self['stock'] = self.__sm[str(stock_symbol_)]
+            self['timestamp'] = Timestamp(timestamp_)
+            self['quantity'] = int(quantity_)
+            self['buy_or_sell'] = bool(buy_or_sell_)
+            self['trade_price'] = int(trade_price_)
+        except (KeyError, ValueError) as error:
+            raise TradeException(str(error))
+        
+        if not self.valid():
+            raise TradeException('Non-valid input')
+            
+    @classmethod
+    def create_trade(cls, stock_symbol_, quantity_, buy_or_sell_, trade_price_):
+        '''
+        Trade is created by using the current time
+        
+        @stock_symbol_ - Symbol of the given stock
+        @quantity_ - Quantity of the stock
+        @buy_or_sell_ - The stocks are bought or sold
+        @trade_price_ - The price of the stocks
+        '''
+        return cls(stock_symbol_, Timestamp.now(), quantity_, buy_or_sell_, trade_price_)
+    
+    def valid(self):
+        '''
+        For validation
+        '''
+        return self['quantity'] > 0 and self['trade_price'] > 0
+        
+    
+class _SingletonTradeManager(type):
+    '''
+    The meta class of Trade Manager.
+    This is an elegant way to handle singletons.
+    '''
+    
+    _instance = None
     def __call__(cls, *args, **kwargs):
         if cls._instance == None:
             cls._instance = super(_SingletonTradeManager, cls).__call__()
         return cls._instance 
 
 class TradeManager(metaclass=_SingletonTradeManager):
+    '''
+    Trade Manager class.
+    1) Meta classes: _SingletonStockManager
+    2) Singleton
+    '''
     def __init__(self):
+        '''
+        Constructor for initialisation
+        
+        Because dataframe creation is time consuming, we used timestamps for 
+        checking if the previous dataframe is valid or not
+        '''
+        
         super(TradeManager, self).__init__()
         self.__storage = []
         
+        self.__df_storage = None
+        self.__df_time = None
+        self.__append_time = None
+        
     def add(self, trade_):
-        self.__storage.append(trade_)
+        '''
+        adding a new trade
+        
+        @trade_ - The trade
+        
+        Note: appending time is saved for dataframe validation
+        '''
+        if isinstance(trade_, Trade):
+            self.__storage.append(trade_)
+            self.__append_time = Timestamp.now()
+        else:
+            raise TradeManagerException('Invalid trade')
+        
+    def __len__(self):
+        return len(self.__storage)
         
     def volume_weighted_stock_price(self, interval_):
+        '''
+        Formula : \frac{\sum{i}{trade_price_i * qunatity_i}}{\sum{i}{quantity_i}}
+        
+        '''
         try:
             #unfortunately, pandas' DataFrame cannot be appended "in place".
-            df = DataFrame(self.__storage)
-            df.set_index('timestamp')
+            if self.__df_time == None or self.__append_time > self.__df_time:
+                self.__df_storage = DataFrame(self.__storage)
+                self.__df_storage.set_index('timestamp')
+                self.__df_time = Timestamp.now()
             
-            current_df = df[df.timestamp >= Timestamp.now('CET')-Timedelta(seconds = int(interval_))]
+            current_df = self.__df_storage[self.__df_storage.timestamp >= Timestamp.now()-Timedelta(seconds = int(interval_))]
             return sum(current_df.quantity * current_df.trade_price)/sum(current_df.quantity)
-        except TypeError:
-            raise TradeManagerException('Invalid time interval')
+        except TypeError as te:
+            raise TradeManagerException('Invalid time interval: {0}'.format(te))
     
-    def gbce_all_share_index():
-        return array([trade['quantity']*trade['trade_price'] for trade in self.__storage]).prod()**(1.0/len(self.__storage))
+    def gbce_all_share_index(self):
+        '''
+        Geometric mean for stock prices
+        
+        RELATIONSHIP WITH LOGARITHMS
+        https://en.wikipedia.org/wiki/Geometric_mean#Relationship_with_logarithms
+        '''
+        try:
+            return exp(sum([trade['quantity']*log(trade['trade_price']) for trade in self.__storage])/sum([trade['quantity'] for trade in self.__storage]))
+        except ZeroDivisionError:
+            return 0.0
+        
+    @classmethod
+    def _clear(cls):
+        TradeManager._instance = None
     
         
     
